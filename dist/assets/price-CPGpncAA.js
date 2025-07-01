57fdfdf5fb2a09aa1abec098f3dd3821be6b38bc
Alireza JavadZadeh <alirezajavadzade76@gmail.com>
2025-07-01 22:48:02 +0330

    remove rule

---
+++ dist/assets/price-CPGpncAA.js
@@ -0,0 +1,1 @@
+import{Q as e}from"./index-Dc0Fp47x.js";const s={async GoldPrice(){return(await e.get("/goldPrice")).data}};export{s as P};
--- dist/assets/price-HGc-TJPT.js
+++
@@ -1,1 +0,0 @@
-import{Q as e}from"./index-D-Fm76kd.js";const s={async GoldPrice(){return(await e.get("/goldPrice")).data}};export{s as P};
