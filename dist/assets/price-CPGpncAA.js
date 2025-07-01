a7923cc4e164e6845f49366dc337ddf374fed37d
Alireza JavadZadeh <alirezajavadzade76@gmail.com>
2025-07-01 22:56:11 +0330

    remove rule & num to word

--- dist/assets/price-CPGpncAA.js
+++
@@ -1,1 +0,0 @@
-import{Q as e}from"./index-Dc0Fp47x.js";const s={async GoldPrice(){return(await e.get("/goldPrice")).data}};export{s as P};
---
+++ dist/assets/price-HGc-TJPT.js
@@ -0,0 +1,1 @@
+import{Q as e}from"./index-D-Fm76kd.js";const s={async GoldPrice(){return(await e.get("/goldPrice")).data}};export{s as P};
